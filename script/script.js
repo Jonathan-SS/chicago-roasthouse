9daf1403bc275065d817fca6745e96a91a7dadc9
majajohannsen <johannsen12@hotmail.com>
2020-05-23 15:32:51 +0200

    version 34

--- script/script.js
+++ script/script.js
@@ -3,25 +3,23 @@
 
 
 //forside video knap
-const vid=document.getElementById("video")
-const knap=document.getElementById("playknap")
-const pause=document.getElementById("pauseknap")
+const vid = document.getElementById("video")
+const knap = document.getElementById("playknap")
+const pause = document.getElementById("pauseknap")
 
 
 
-knap.addEventListener("click", function(){
-	vid.play();
-	knap.classList.toggle("gem");
-	pause.classList.toggle("vis");
-
-
+knap.addEventListener("click", function () {
+    vid.play();
+    knap.classList.toggle("gem");
+    pause.classList.toggle("vis");
 
 });
 
-pause.addEventListener("click",function(){
-	vid.pause();
-	knap.classList.toggle("gem");
-	pause.classList.toggle("vis");
+pause.addEventListener("click", function () {
+    vid.pause();
+    knap.classList.toggle("gem");
+    pause.classList.toggle("vis");
 
 
 });
@@ -35,10 +33,9 @@ pause.addEventListener("click",function(){
 /*dopup lavet med inspiration fra:https://www.w3schools.com/howto/howto_js_dropdown.asp*/
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-const dropin=document.getElementById("mydropdown")
+const dropin = document.getElementById("mydropdown")
 
 function klikfunktion() {
-	
-  dropin.classList.toggle("show");
-}
 
+    dropin.classList.toggle("show");
+}
