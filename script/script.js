0516ca0257a8c4ccfd4ea8d198be4fd0df988eda
Jonathan-SS <56107465+Jonathan-SS@users.noreply.github.com>
2020-05-23 08:17:02 +0200

    alle nav done

--- script/script.js
+++ script/script.js
@@ -4,9 +4,8 @@
 
 
 
-
-
-
+//drop up menu
+/*dopup lavet med inspiration fra:https://www.w3schools.com/howto/howto_js_dropdown.asp*/
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 const dropin=document.getElementById("mydropdown")
@@ -14,18 +13,4 @@ const dropin=document.getElementById("mydropdown")
 function klikfunktion() {
 	
   dropin.classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
 }
